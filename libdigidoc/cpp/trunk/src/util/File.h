f450a9cde15072c0521814f17b8f70d54143f7e5
janno <janno@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-02-27 15:14:27 +0000

    * SHA256 digest works * XAdES SignedInfo->Reference attr Type value chaged to "http://uri.etsi.org/01903#SignedProperties" (was http://uri.etsi.org/01903/#SignedProperties) * check IssuerCertHash by method specified in signature document * EncapsulatedOCSPValue should be hash of OCSP response, not hash of SignatureValue * File digest was calculated from 0 bytes in some cases although file was not empty * Signing certificate is verified * All tests run under Ubuntu/Debian and WinXP

---
+++ libdigidoc/cpp/trunk/src/util/File.h
@@ -0,0 +1,44 @@
+#if !defined(__FILE_H_INCLUDED__)
+#define __FILE_H_INCLUDED__
+
+#include <string>
+
+#include "../io/IOException.h"
+
+namespace digidoc
+{
+    namespace util
+    {
+
+        /**
+         * Implements common file-system operations for current platform
+         *
+         * @author Janari Põld
+         */
+        class File
+        {
+          public:
+              static bool fileExists(const std::string& path);
+              static bool directoryExists(const std::string& path);
+              static std::string fileName(const std::string& path);
+              static std::string directory(const std::string& path);
+              static std::string path(const std::string& directory, const std::string& relativePath, bool unixStyle = false);
+              static std::string tempFileName();
+              static std::string tempDirectory();
+              static void createDirectory(const std::string& path) throw(IOException);
+              static std::string createTempDirectory() throw(IOException);
+              static unsigned long fileSize(const std::string& path) throw(IOException);
+              static std::vector<std::string> listFiles(const std::string& directory, bool relative = false,
+            		  bool listEmptyDirectories = false, bool unixStyle = false) throw(IOException);
+              static void copyFile(const std::string& srcPath, const std::string& destPath, bool overwrite = true) throw(IOException);
+              static void moveFile(const std::string& srcPath, const std::string& destPath, bool overwrite = true) throw(IOException);
+
+        private:
+              static std::vector<std::string> getDirSubElements(const std::string& directory, bool relative,
+            		  bool filesOnly, bool unixStyle) throw(IOException);
+        };
+
+    }
+}
+
+#endif // !defined(__FILE_H_INCLUDED__)
