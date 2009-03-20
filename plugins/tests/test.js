2110e1c5b7ca5730a691538e1c53e83635de2648
anttix <anttix@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-03-20 17:27:28 +0000

    Browser Plugin tests: Fixed Javascript on IE. Added crashtests and other improvements.

--- plugins/tests/test.js
+++ plugins/tests/test.js
@@ -4,6 +4,42 @@ var names = [ "firstName", "lastName", "middleName", "sex",
               "comment1", "comment2", "comment3", "comment4"
 ];
 
+function inittest() {
+    var e = document.getElementById("persData");
+    for(var i in names) {
+        var r = e.insertRow(e.rows.length);
+        var el = document.createElement('span');
+	el.id = "s" + names[i];
+	r.insertCell(0).innerHTML = names[i];
+	r.insertCell(1).appendChild(el);
+    }
+
+    // FIXME: Seda jama ei peaks vaja olema!
+    if(typeof(esteid) == 'undefined') {
+        if(!window.external) return;
+        esteid = window.external;
+    }
+
+    var e = document.getElementById("eidver");
+    try {
+        e.innerHTML = esteid.getVersion();
+	e.style.background = "green";
+    }
+    catch(err) {
+        e.innerHTML = "Error: " + err.message;
+	e.style.background = "red";
+    }
+    try {
+        esteid.addEventListener("cardInsert", cardInserted);
+        esteid.addEventListener("cardRemove", cardRemoved);
+    }
+    catch(err) {
+        var e = document.getElementById("cardstatus");
+        e.innerHTML = "Error: " + err.message;
+        e.style.background = "red";
+    }
+}
+
 function cardInserted(reader) {
     var e = document.getElementById("cardstatus");
     e.innerHTML = "Sees";
@@ -28,7 +64,7 @@ function readPersonalData() {
     }
     catch(err) {
         clearPersonalData();
-        stat.innerHTML = "Error: " + err;
+        stat.innerHTML = "Error: " + err.message;
         stat.style.background = "red";
     }
 }
@@ -40,32 +76,6 @@ function clearPersonalData() {
     }
 }
 
-function inittest() {
-    var e = document.getElementById("persData");
-    for(var i in names) {
-        e.innerHTML += '<tr><td>' + names[i] + '</td><td><span id="s' +
-                       names[i] + '"></span></td></tr>';
-    }
-
-    clearPersonalData();
-
-    // FIXME: Seda jama ei peaks vaja olema!
-    if(!esteid) {
-        if(!window.external) return;
-        esteid = window.external;
-    }
-
-    var e = document.getElementById("eidver");
-    try {
-        e.innerHTML = esteid.getVersion();
-    }
-    catch(err) {
-        e.innerHTML = "Error: " + err;
-    }
-    esteid.addEventListener("cardInsert", cardInserted);
-    esteid.addEventListener("cardRemove", cardRemoved);
-}
-
 function testPersonalData() {
     readPersonalData();
 }
@@ -73,14 +83,48 @@ function testPersonalData() {
 function testSign() {
     var e = document.getElementById("signstatus");
     try {
-        signedData = esteid.sign("0A0B0C0D",0);
+        var certList = esteid.getCertificateList();
+        var certArr = certList.split(",");
+        var cert= esteid.getCertificateByThumbprint(certArr[1]);
+        signedData = esteid.sign("0A0B0C0D",cert);
+        //signedData = esteid.sign("0A0B0C0D",0);
         e.innerHTML = "OK: " + signedData;
         e.style.background = "";
     }
     catch(err) {
-        e.innerHTML = "Error: " + err;
+        e.innerHTML = "Error: " + err.message;
         e.style.background = "red";
     }
+}
+
+function runCrashCode(code) {
+    var e = document.getElementById("crashstatus");
+    try {
+        eval(code);
+	e.innerHTML += '<span class="red">' + code + " did not throw exception</span>";
+    }
+    catch(err) {
+        e.innerHTML += code + ": " + err.message;
+    }
+    e.innerHTML += "<br />";
+}
+
+function testCrash() {
+    var e = document.getElementById("crashstatus");
+    e.innerHTML = "";
+    runCrashCode('esteid.addEventListener(null, null);');
+    runCrashCode('esteid.addEventListener("huinamuina", null);');
+    runCrashCode('esteid.addEventListener("", null);');
+    runCrashCode('esteid.addEventListener("cardInsert", null);');
+    runCrashCode('esteid.addEventListener("cardRemove", "");');
+    runCrashCode('esteid.addEventListener("cardRemove", 0);');
+    runCrashCode('esteid.getCertificateByThumbprint(null);');
+    runCrashCode('esteid.getCertificateByThumbprint(0);');
+    runCrashCode('esteid.getCertificateByThumbprint("");');
+    runCrashCode('esteid.sign(null, null);');
+    runCrashCode('esteid.sign("","");');
+    runCrashCode('esteid.sign(0,0);');
+    runCrashCode('esteid.sign("",0);');
 }
 
 function testWhatever() {
