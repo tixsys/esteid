7c60876f586a3aa2f3f383647bf59aab72e1391e
raul <raul@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-05-14 15:17:40 +0000

    Add translations to fix build

---
+++ qdigidoccrypto/trunk/src/translations/et.ts
@@ -0,0 +1,409 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="et">
+<context>
+    <name>CryptDoc</name>
+    <message>
+        <source>Container is not open</source>
+        <translation type="unfinished">Ümbrik ei ole avatud</translation>
+    </message>
+    <message>
+        <source>Container is encrypted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to add file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to calculate digest</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to add key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to create document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recipient does not exist in document recipient list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed decrypt data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to read decrypted data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No recipiest keys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to encrypt data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to open crypted document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Missing document</source>
+        <translation type="unfinished">Puuduv dokument</translation>
+    </message>
+    <message>
+        <source>Failed to remove file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Missing key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to remove key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Container is not crypted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to save encrpyted file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to save file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KeyAddDialog</name>
+    <message>
+        <source>
+Error code: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add Recipient</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SK Certificates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SS number:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Issuer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Expiry date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cert store</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add to recipies list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KeyDialog</name>
+    <message>
+        <source>Recipient</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Crypt method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Expires</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Issuer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Crypt recipient</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Attribute</source>
+        <translation type="unfinished">Atribuut</translation>
+    </message>
+    <message>
+        <source>Value</source>
+        <translation type="unfinished">Väärtus</translation>
+    </message>
+    <message>
+        <source>Decrypt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show certificate</source>
+        <translation type="unfinished">Näita sertifikaati</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished">Katkesta</translation>
+    </message>
+</context>
+<context>
+    <name>KeyWidget</name>
+    <message>
+        <source>Show details</source>
+        <translation type="unfinished">Vaata üksikasju</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LdapSearch</name>
+    <message>
+        <source>Failed to init ldap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to init ldap search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to get result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Empty result</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Settings</source>
+        <translation>Seaded</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Abi</translation>
+    </message>
+    <message>
+        <source>Estonian</source>
+        <translation>Estonian</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <source>Russian</source>
+        <translation>Russian</translation>
+    </message>
+    <message>
+        <source>German</source>
+        <translation>German</translation>
+    </message>
+    <message>
+        <source>Latvian</source>
+        <translation>Latvian</translation>
+    </message>
+    <message>
+        <source>Lithuanian</source>
+        <translation>Lithuanian</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Katkesta</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Edasi</translation>
+    </message>
+    <message>
+        <source>Add file</source>
+        <translation>Lisa fail</translation>
+    </message>
+    <message>
+        <source>Send container to email</source>
+        <translation>Saada allkirjutatud ümbrik e-postiga</translation>
+    </message>
+    <message>
+        <source>Browse container location</source>
+        <translation>Näita allkirjutatud ümbriku asukohta kettal</translation>
+    </message>
+    <message>
+        <source>Save files to disk</source>
+        <translation>Salvesta failid kettale</translation>
+    </message>
+    <message>
+        <source>Open container</source>
+        <translation>Ava ümbrik</translation>
+    </message>
+    <message>
+        <source>Select folder where files will be stored</source>
+        <translation>Vali kataloog kuhu failid salvestatakse</translation>
+    </message>
+    <message>
+        <source>Container: &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Ümbrik: &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Select documents</source>
+        <translation>Lisa failid</translation>
+    </message>
+    <message>
+        <source>Remove checked files</source>
+        <translation>Eemalda valitud</translation>
+    </message>
+    <message>
+        <source>Save file</source>
+        <translation>Salvesta fail</translation>
+    </message>
+    <message>
+        <source>Are you sure you want remove files %1</source>
+        <translation>Oled sa kindel, et tahad eemaldada faile %1?</translation>
+    </message>
+    <message>
+        <source>Filename</source>
+        <translation>Faili nimi</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Suurus</translation>
+    </message>
+    <message>
+        <source>Failed to send email</source>
+        <translation>Ekirja koostamine ebaõnnestus</translation>
+    </message>
+    <message>
+        <source>Documents (*.cdoc)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Decrypt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Encrypt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>
+Error code: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DigiDoc crypt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I want to:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Crypt documents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>View crypted document content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Arial Baltic&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Arial&apos;;&quot;&gt;&lt;a name=&quot;u0&quot;&gt;&lt;/a&gt;&lt;span style=&quot; color:#000000;&quot;&gt;D&lt;/span&gt;&lt;span style=&quot; color:#000000;&quot;&gt;o&lt;/span&gt;&lt;span style=&quot; font-family:&apos;Arial Baltic&apos;; color:#000000;&quot;&gt;kumentide krüptimine&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Arial&apos;;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Arial&apos;; color:#000000;&quot;&gt;&lt;a name=&quot;u2&quot;&gt;&lt;/a&gt;Dokumentide krüptimine on viis turvaliselt edastada dokumendid nii, et ainult valitud saaja või saajad saavad neid avada.&lt;br /&gt;&lt;br /&gt;Id-kaardisertifikaatidega krüptimine ei ole mõeldud andmete pikaajaliseleturvalisele säilitamisele jm ülesannete lahendamiseks, see on ainultdokumentide edastamise viis.&lt;br /&gt;&lt;br /&gt;Dokumendi krüptimisel kasutatakse Sertifitseerimiskeskuse LDAP teenust saaja(te) sertifikaatideotsimiseks. Dokumendid krüptitakse sertifikaadis sisalduva avalikuvõtmega. Krüptimine on ühesuunaline, de-krüptimiseks on vaja vastavasertifikaadi privaatvõtit, mis on salvestatud ainult vastava isikuID-kaardil.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Jah, ma kasutan ID-kaardi sertifikaadiga krüptimist ainult andmete turvaliseks edastamiseks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Container content:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Keys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add recipient</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Selected action requires auth certificate.
+For using auth certificate enter PIN1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Seaded</translation>
+    </message>
+    <message>
+        <source>Container default location</source>
+        <translation>Ümbriku loomise vaikekataloog</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>Select folder</source>
+        <translation>Vali kataloog</translation>
+    </message>
+    <message>
+        <source>Show signing intro</source>
+        <translation>Näita allkirjastamisel sissejuhatust</translation>
+    </message>
+    <message>
+        <source>Same folder</source>
+        <translation>Samasse kataloogi</translation>
+    </message>
+    <message>
+        <source>Ask container name</source>
+        <translation>Küsi dokumendi nime</translation>
+    </message>
+</context>
+</TS>
