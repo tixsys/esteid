b1cbbdcafdead1b471cb8a96c7a71daf7569923a
jargo <jargo@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-05-29 14:34:32 +0000

    qesteidutil: added russian qt translation files

---
+++ qesteidutil/trunk/src/translations/ru.ts
@@ -0,0 +1,332 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
+<context>
+    <name>CertificateWidget</name>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="91"/>
+        <location filename="../CertificateWidget.cpp" line="107"/>
+        <source>Save certificate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="96"/>
+        <source>Certificates (*.pem *.crt *.cer)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="107"/>
+        <source>Failed to save file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="116"/>
+        <source>Certificate Information:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="118"/>
+        <source>This certificate is intended for following purpose(s):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="127"/>
+        <source>Issued to:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="129"/>
+        <source>Issued by:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="131"/>
+        <location filename="../CertificateWidget.cpp" line="154"/>
+        <source>Valid from</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="132"/>
+        <source>to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="136"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="137"/>
+        <source>Serial number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="140"/>
+        <source>Signature algorithm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="153"/>
+        <source>Issuer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="155"/>
+        <source>Vaild to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="169"/>
+        <source>Subject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="170"/>
+        <source>Public key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CertificateWidget.cpp" line="172"/>
+        <source>Certificate policies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="14"/>
+        <source>Certificate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="24"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="34"/>
+        <source>Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="44"/>
+        <source>Field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="49"/>
+        <source>Value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="84"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="94"/>
+        <location filename="../ui/CertificateWidget.ui" line="100"/>
+        <source>Certification Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CertificateWidget.ui" line="121"/>
+        <source>Certificate status:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DiagnosticsDialog</name>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="54"/>
+        <source>Version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="57"/>
+        <source>OS:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="84"/>
+        <source>Library paths:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="87"/>
+        <source>Libraries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="117"/>
+        <source>Smart Card service status: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="119"/>
+        <source>PCSC service status: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="121"/>
+        <source>Running</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="121"/>
+        <source>Not running</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="124"/>
+        <source>Card readers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="139"/>
+        <source>%1 - failed to get version info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="161"/>
+        <source>ID - %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="167"/>
+        <source>Error reading card data:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="211"/>
+        <source>Save as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="213"/>
+        <source>Text files (*.txt)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="219"/>
+        <source>Error occured</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DiagnosticsDialog.cpp" line="219"/>
+        <source>Failed write to file!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DiagnosticsDialog.ui" line="26"/>
+        <source>Diagnostics</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JsExtender</name>
+    <message>
+        <location filename="../jsextender.cpp" line="115"/>
+        <source>Authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jsextender.cpp" line="115"/>
+        <source>This action needs authentication
+Enter PIN1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jsextender.cpp" line="262"/>
+        <source> - %1 (active)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jsextender.cpp" line="262"/>
+        <source> - %1 (not active)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jsextender.cpp" line="330"/>
+        <source>Save picture</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jsextender.cpp" line="330"/>
+        <source>JPEG (*.jpg *.jpeg);;PNG (*.png);;TIFF (*.tif *.tiff);;24-bit Bitmap (*.bmp)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="69"/>
+        <source>ID-card utility</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../SslCertificate.cpp" line="87"/>
+        <source>Proves your identity to a remote computer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SslCertificate.cpp" line="89"/>
+        <source>Protects e-mail messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SslCertificate.cpp" line="91"/>
+        <source>OCSP signing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SslCertificate.cpp" line="98"/>
+        <source>All application policies</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="30"/>
+        <source>Save password for specified period in minutes
+0 - always ask</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="41"/>
+        <source>Check updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="71"/>
+        <source>Update automatically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="104"/>
+        <source>Once a day</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="109"/>
+        <source>Once a week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="114"/>
+        <source>Once a month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="119"/>
+        <source>Never</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
