b8914549b81d3266fd886f165a9435f11b658aea
raul <raul@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-05-30 11:25:14 +0000

    qdigidocclient: fix

--- qdigidocclient/trunk/src/version.h
+++ qdigidocclient/trunk/src/version.h
@@ -1,7 +1,18 @@
-#define BUILD_VER	0
+#ifndef MAJOR_VER
 #define MAJOR_VER	0
+#endif
+
+#ifndef MINOR_VER
 #define MINOR_VER	0
+#endif
+
+#ifdef RELEASE_VER
 #define RELEASE_VER	0
+#endif
+
+#ifndef BUILD_VER
+#define BUILD_VER	0
+#endif
 
 #define FILE_VER	MAJOR_VER,MINOR_VER,RELEASE_VER,BUILD_VER
 #define FILE_VER_DOT	MAJOR_VER.MINOR_VER.RELEASE_VER.BUILD_VER
