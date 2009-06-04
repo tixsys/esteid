ac317e6800343d181076a6eb70e7f2e543568fb4
janek <janek@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-06-04 16:48:25 +0000

    Improved nightly build script

--- macosx/trunk/Build.sh
+++ macosx/trunk/Build.sh
@@ -2,14 +2,28 @@
 
 svn update ../..
 
-REVISION=`svn info -r HEAD | grep Revision | cut -d ' ' -f 2`
 ARCH=universal
-NAME=`date "+installer_${ARCH}_%Y%m%d_r${REVISION}.dmg"`
-FILE=build/Packages/${NAME}
+REVISION=`svn info -r HEAD | grep Revision | cut -d ' ' -f 2`
 
 if test -n "$1"; then
 	ARCH=$1 # universal, ppc or i386
 fi
+
+CSUM_FILE=build/Build_${ARCH}.txt
+CSUM_NEW=${ARCH}${REVISION}
+CSUM_OLD=${ARCH}0
+
+if test -f ${CSUM_FILE}; then
+	CSUM_OLD=`cat ${CSUM_FILE}`
+fi
+
+if test "${CSUM_OLD}" = "${CSUM_NEW}"; then
+	echo "Build is up to date (revision: ${REVISION}, architecture: ${ARCH})"
+	exit 0
+fi
+
+NAME=`date "+installer_${ARCH}_%Y%m%d_r${REVISION}.dmg"`
+FILE=build/Packages/${NAME}
 
 ./make.rb -V --arch ${ARCH} installer
 
@@ -19,3 +33,5 @@ setfile -a E ${FILE}
 if test -n "$2"; then
 	scp ${FILE} $2
 fi
+
+echo "${CSUM_NEW}" > ${CSUM_FILE}
