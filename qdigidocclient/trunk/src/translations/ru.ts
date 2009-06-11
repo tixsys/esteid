2ea041967085264bb37776c0fda14403b815c313
edgar <edgar@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-06-11 14:03:57 +0000

    updated

--- qdigidocclient/trunk/src/translations/ru.ts
+++ qdigidocclient/trunk/src/translations/ru.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="et">
+<TS version="2.0" language="ru">
 <context>
     <name>CertificateDialog</name>
     <message>
