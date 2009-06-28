8ba2e4bbf84e86de88a0a0db55e48d35590bc325
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-06-28 07:24:15 +0000

    qesteidutil: add missing files to cardlib

---
+++ qesteidutil/trunk/cardlib/types.h
@@ -0,0 +1,18 @@
+#ifndef _cardlib_types_h
+#define _cardlib_types_h
+
+#ifndef UNUSED_ARG
+#define UNUSED_ARG(x) (void)x
+#endif
+
+typedef unsigned int uint;
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef unsigned long dword;
+typedef unsigned short ushort;
+/// shorthand for std::vector<unsigned char>, used everywhere
+typedef	std::vector<byte> ByteVec;
+/// ostream printer
+std::ostream& operator<<(std::ostream &out,const ByteVec &vec);
+
+#endif
