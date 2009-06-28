cf3bcb430209765b59b9e9a363fb2f29c7466d6e
jargo <jargo@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-06-28 13:51:44 +0000

    qesteidutil: more readable cardlib errors

--- qesteidutil/trunk/cardlib/SCError.cpp
+++ qesteidutil/trunk/cardlib/SCError.cpp
@@ -18,8 +18,11 @@ SCError::SCError(long err) : runtime_error("smart card API error"),error(err)
 		case 0x80100069 : //SCARD_W_REMOVED_CARD
 			buf << "No card in specified reader"; break;
 		case 0x80100068 : //SCARD_W_RESET_CARD
+			buf << "The smart card has been reset, so any shared state information is invalid"; break;
 		case 0x80100011 : //SCARD_E_INVALID_VALUE .. needs trapping
-			buf << "Another application is using the card";break;
+			buf << "Another application is using the card"; break;
+		case 0x8010000f : //SCARD_E_PROTO_MISMATCH
+			buf << "The requested protocols are incompatible with the protocol currently in use with the smart card"; break;
 		default:
 			buf << "exception:'" << runtime_error::what() << 
 				"' code:'0x" <<std::hex << std::setfill('0') <<
