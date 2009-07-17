ce70dcda85755952fb2927294a8b3bf5908a8505
manuel <manuel@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-07-17 11:31:17 +0000

    code cleanup

--- plugins/acrobat/trunk/EstEIDReader/EstEIDIncludes.h
+++ plugins/acrobat/trunk/EstEIDReader/EstEIDIncludes.h
@@ -14,31 +14,19 @@
 #define ESTEID_OK 0
 #define ESTEID_ERR 1
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
+#include <string.h>
 #include <opensc/pkcs11.h>
-#include <opensc/pkcs11-opensc.h>
-#include <compat_getpass.h>
 
 #ifdef ENABLE_OPENSSL
-#include "openssl/evp.h"
 #include "openssl/x509.h"
-#include "openssl/rsa.h"
-#include "openssl/bn.h"
-#include "openssl/err.h"
 #endif
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-//extern "C"
-//{
-    void *C_LoadModule(const char *name, CK_FUNCTION_LIST_PTR_PTR);
-    CK_RV C_UnloadModule(void *module);
-//}
+void *C_LoadModule(const char *name, CK_FUNCTION_LIST_PTR_PTR);
+CK_RV C_UnloadModule(void *module);
 
 struct PKCS11Cert
 {
