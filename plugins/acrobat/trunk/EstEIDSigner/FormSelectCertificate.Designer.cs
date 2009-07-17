9df5d53f7b1a4908fb344ecdd6246cf9ef32811f
manuel <manuel@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-07-17 11:35:51 +0000

    custom X509 certificate selection from

---
+++ plugins/acrobat/trunk/EstEIDSigner/FormSelectCertificate.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace EstEIDSigner
+{
+    partial class FormSelectCertificate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSelectCertificate));
+            this.Ok = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Message = new System.Windows.Forms.Label();
+            this.listCertificates = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // Ok
+            // 
+            this.Ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Ok.Location = new System.Drawing.Point(366, 178);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 0;
+            this.Ok.Text = "Vali";
+            this.Ok.UseVisualStyleBackColor = true;
+            this.Ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.Location = new System.Drawing.Point(447, 178);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 1;
+            this.Cancel.Text = "Katkesta";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(12, 9);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(50, 13);
+            this.Message.TabIndex = 2;
+            this.Message.Text = "Message";
+            // 
+            // listCertificates
+            // 
+            this.listCertificates.Location = new System.Drawing.Point(15, 34);
+            this.listCertificates.MultiSelect = false;
+            this.listCertificates.Name = "listCertificates";
+            this.listCertificates.Size = new System.Drawing.Size(507, 138);
+            this.listCertificates.TabIndex = 3;
+            this.listCertificates.UseCompatibleStateImageBehavior = false;
+            this.listCertificates.View = System.Windows.Forms.View.Details;
+            // 
+            // FormSelectCertificate
+            // 
+            this.AcceptButton = this.Ok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.Cancel;
+            this.ClientSize = new System.Drawing.Size(534, 213);
+            this.Controls.Add(this.listCertificates);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Ok);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormSelectCertificate";
+            this.Text = "FormSelectCertificate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.ListView listCertificates;
+    }
+}
