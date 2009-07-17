f29e216b97d2b7295031818337d0b0cb8fbec05e
manuel <manuel@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-07-17 11:28:43 +0000

    session class

---
+++ plugins/acrobat/trunk/EstEIDReader/EstEIDSession.cpp
@@ -0,0 +1,46 @@
+#include "EstEIDIncludes.h"
+#include "EstEIDSession.h"
+
+EstEIDSession::EstEIDSession(void)
+{
+    m_module = NULL;
+    m_p11 = NULL;
+    m_initialized = 0;
+}
+
+CK_RV EstEIDSession::Open(const char *module)
+{
+    CK_RV rv = ESTEID_ERR;
+
+    m_module = C_LoadModule( module, &m_p11 );
+    if( m_module == NULL )
+        return( ESTEID_ERR );
+
+    rv = m_p11->C_Initialize( NULL );
+    if( rv != CKR_OK )
+    {
+        m_p11->C_Finalize( NULL );
+        C_UnloadModule( m_module );
+        m_module = NULL;
+        return( rv );
+    }
+
+    m_initialized = 1;
+
+    return( rv );
+}
+
+CK_RV EstEIDSession::Close()
+{
+    CK_RV rv = ESTEID_ERR;
+
+    if( m_p11 != NULL )
+    {
+        rv = m_p11->C_Finalize( NULL );
+        C_UnloadModule( m_module );
+    }
+
+    m_initialized = 0;
+
+    return( rv );
+}
