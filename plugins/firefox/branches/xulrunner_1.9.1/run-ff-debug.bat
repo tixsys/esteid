ab0aec68e3a2c82dd66c05b5065783168c60003c
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-07-19 13:44:16 +0000

    plugins/firefox: remove branch xulrunner_1.9.1

--- plugins/firefox/branches/xulrunner_1.9.1/run-ff-debug.bat
+++
@@ -1,6 +0,0 @@
-rem Close all Firefox windows!
-set NSPR_LOG_MODULES=nsNativeModuleLoader:5,Plugin:5
-set NSPR_LOG_FILE=c:\firefox.log
-rem set NSPR_LOG_MODULES=all:5
-
-"c:\Program Files (x86)\Mozilla Firefox\firefox.exe"
