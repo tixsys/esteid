ce09816d241d91ba03f5da67d38241fe52672c59
janek <janek@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-07-22 18:38:17 +0000

    macosx: fixed version suffix

--- macosx/trunk/Make.rb
+++ macosx/trunk/Make.rb
@@ -814,7 +814,7 @@ class Application
 				:identifier => 'org.esteid.installer.tokend.10.5',
 				:location => '/System/Library/Security/tokend',
 				:script => "return system.version.ProductVersion >= '10.5';",
-				:system => '>=10.5'
+				:system => '10.5>='
 			}, {
 				:name => 'esteid-tokend-tiger',
 				:files => File.join(@options.binaries, '10.4', 'OpenSC.tokend'),
@@ -822,7 +822,7 @@ class Application
 				:identifier => 'org.esteid.installer.tokend.10.4',
 				:location => '/System/Library/Security/tokend',
 				:script => "return system.version.ProductVersion &lt; '10.5';",
-				:system => '&lt;10.5'
+				:system => '10.5&lt;'
 			}, {
 				:name => 'esteid',
 				:title => 'ID-card',
