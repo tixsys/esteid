77250dc8bbe55d192ed4ed3757f4ccf6d45f9117
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-07-27 12:12:26 +0000

    Rename EsteidCustomAction project to EsteidWixCustomAction

--- packages/trunk/windows/EsteidWixCustomAction/stdafx.cpp
+++ packages/trunk/windows/EsteidWixCustomAction/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// EsteidCustomAction.pch will be the pre-compiled header
+// EsteidWixCustomAction.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
