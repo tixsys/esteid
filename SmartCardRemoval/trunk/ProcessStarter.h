6c155b5960ee01779f7e86d3a511fe5c60c9560e
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-09-17 10:32:59 +0000

    SmartCardRemoval: added svn:external smartcard++ and updated vcproj to compile with it

---
+++ SmartCardRemoval/trunk/ProcessStarter.h
@@ -0,0 +1,36 @@
+// http://www.codeproject.com/KB/vista-security/interaction-in-vista.aspx
+
+#ifndef _PROCESS_STARTER_H_
+#define _PROCESS_STARTER_H_
+
+#ifndef _DEBUG
+#define NOLOG
+#endif
+
+#include <string>
+#include <sstream>
+#include <fstream>
+
+class ProcessStarter
+{
+	typedef void *_handle;
+	typedef _handle *_phandle;
+public:
+    ProcessStarter(const std::string& processPath, const std::string& arguments = "");
+    bool Run(bool forceRun = false);
+    
+	_phandle GetCurrentUserToken();
+	_phandle GetCurrentUserTokenOld();
+	unsigned long getShellProcessPID();
+
+private:
+    std::string processPath_;
+    std::string arguments_;
+#ifdef NOLOG
+	std::stringstream log;
+#else
+	std::ofstream log;
+#endif
+};
+
+#endif //_PROCESS_STARTER_H_
