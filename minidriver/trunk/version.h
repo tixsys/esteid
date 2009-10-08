6c6ed4012d1c14b0b4ba2e0feac0e7d8079301bd
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-10-08 17:46:47 +0000

    minidriver: .inf file updates and bump version

--- minidriver/trunk/version.h
+++ minidriver/trunk/version.h
@@ -1,4 +1,4 @@
-#define FILE_VERSION         1,1,0,10
-#define PRODUCT_VERSION      1,1,0,10
-#define FILE_VERSION_STR     "1, 1, 0, 10\0"
-#define PRODUCT_VERSION_STR  "1, 1, 0, 10\0"
+#define FILE_VERSION         1,1,0,11
+#define PRODUCT_VERSION      1,1,0,11
+#define FILE_VERSION_STR     "1, 1, 0, 11\0"
+#define PRODUCT_VERSION_STR  "1, 1, 0, 11\0"
