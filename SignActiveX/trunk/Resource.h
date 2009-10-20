9f551514c362190a487262898d31577a086425b0
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-10-20 13:50:47 +0000

    SignActiveX: Initial import of old code which was released under LGPLv3

---
+++ SignActiveX/trunk/Resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by EIDCard.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_SIGN                        102
+#define IDR_ESTEID                      103
+#define IDD_DLG_UNIT                    220
+#define IDC_GROUPBOX                    221
+#define IDC_LISTVIEW                    222
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        206
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         202
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
