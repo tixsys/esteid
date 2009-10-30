3554e04f591bce11db0550dddc2e1d9672d9fb7a
mark <mark@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-10-30 16:33:39 +0000

    cmake: Mac support added for OOo plugin

--- cmake/modules/FindOOoSDK.cmake
+++ cmake/modules/FindOOoSDK.cmake
@@ -30,22 +30,34 @@ IF (UNIX)
 	SET(OOSDK_SEARCH_DIR "${OO_SDK_URE_HOME}/lib")
 ENDIF (UNIX)
 
+IF (APPLE)
+	SET(OOSDK_SEARCH_DIR "/Users/mark/OpenOffice.org3.1_SDK/macosx/lib")
+ENDIF (APPLE)
+
 message( STATUS "searching libraries in ${OOSDK_SEARCH_DIR}")
 
-SET(CPPU_NAMES icppu uno_cppu cppu)
+SET(CPPU_NAMES icppu uno_cppu cppu libuno_cppu)
 FIND_LIBRARY(CPPU_LIBRARY NAMES ${CPPU_NAMES} PATHS ${OOSDK_SEARCH_DIR})
 
-SET(CPPUHELPER_NAMES uno_cppuhelpergcc3 icppuhelper cppuhelper)
+SET(CPPUHELPER_NAMES uno_cppuhelpergcc3 icppuhelper cppuhelper libuno_cppuhelpergcc3)
 FIND_LIBRARY(CPPUHELPER_LIBRARY NAMES ${CPPUHELPER_NAMES} PATHS ${OOSDK_SEARCH_DIR})
 
-SET(SAL_NAMES isal uno_sal sal)
+SET(SAL_NAMES isal uno_sal sal libuno_sal)
 FIND_LIBRARY(SAL_LIBRARY NAMES ${SAL_NAMES} PATHS ${OOSDK_SEARCH_DIR})
 
-SET(SALHELPER_NAMES isalhelper uno_salhelpergcc3 salhelper)
+SET(SALHELPER_NAMES isalhelper uno_salhelpergcc3 salhelper libuno_salhelpergcc3)
 FIND_LIBRARY(SALHELPER_LIBRARY NAMES ${SALHELPER_NAMES} PATHS ${OOSDK_SEARCH_DIR})
 
-SET(STLPORT_NAMES istlport_vc71 stlport_gcc stlport_vc71)
+SET(STLPORT_NAMES istlport_vc71 stlport_gcc stlport_vc71 libstlport_gcc)
 FIND_LIBRARY(STLPORT_LIBRARY NAMES ${STLPORT_NAMES} PATHS ${OOSDK_SEARCH_DIR})        
+
+IF (APPLE)
+#	SET(CPPU_LIBRARY "${OOSDK_SEARCH_DIR}/libuno_cppu.dylib.3")
+#	SET(CPPUHELPER_LIBRARY "${OOSDK_SEARCH_DIR}/libuno_cppuhelpergcc3.dylib.3")
+#	SET(SAL_LIBRARY "${OOSDK_SEARCH_DIR}/libuno_sal.dylib.3")
+#	SET(SALHELPER_LIBRARY "${OOSDK_SEARCH_DIR}/libuno_salhelpergcc3.dylib.3")
+	SET(STLPORT_LIBRARY "/usr/local/lib/libstlport.dylib")
+ENDIF (APPLE)
 
 # Handle the QUIETLY and REQUIRED arguments and set OOSDK_FOUND to
 # TRUE if all listed variables are TRUE.
