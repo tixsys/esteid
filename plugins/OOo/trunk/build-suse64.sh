380277a1677db8ecfc641dbd9aa6336add15d8b9
mark <mark@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-11-04 09:37:29 +0000

    OOo Extension: Suse64 build fixed

--- plugins/OOo/trunk/build-suse64.sh
+++ plugins/OOo/trunk/build-suse64.sh
@@ -5,6 +5,7 @@ export OFFICE_BASE_PROGRAM_PATH=/usr/lib64/ooo3/basis-link/program
 export OO_SDK_URE_LIB_DIR=/usr/lib64/ooo3/basis-link/ure-link/lib
 export LD_LIBRARY_PATH=/usr/lib64/ooo3/basis-link/ure-link/lib
 export OO_SDK_INC_SUSE=/usr/lib64/ooo3/solver/inc
+export OO_SDK_MAP_SUSE=/usr/lib64/ooo3/solenv/src/component.map
 cd build
 rm -rf *
 cmake ..
