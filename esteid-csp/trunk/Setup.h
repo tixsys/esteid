49c1634181ef974c5b277ed662d51a9d2411c9e2
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-11-19 14:29:24 +0000

    esteid-csp: Bumped version to 2.2.18 for latest smartcard++ changes

--- esteid-csp/trunk/Setup.h
+++ esteid-csp/trunk/Setup.h
@@ -17,10 +17,10 @@
 #define PACKAGE_NAME "EstEID CSP" PKG_BIT PKG_BUILD
 
 /* rc version */
-#define RC_VERSION 2,2,17
+#define RC_VERSION 2,2,18
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.2.17"
+#define PACKAGE_VERSION "2.2.18"
 
 /* Name of package */
 #define PACKAGE "esteidcsp.dll"
