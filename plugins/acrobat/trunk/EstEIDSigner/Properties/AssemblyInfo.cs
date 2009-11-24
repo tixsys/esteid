e9dde9e96acf1f4b2a042141500af4200fbafef1
manuel <manuel@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2009-11-24 18:20:51 +0000

    plugins/acrobat: added Culture based UI

--- plugins/acrobat/trunk/EstEIDSigner/Properties/AssemblyInfo.cs
+++ plugins/acrobat/trunk/EstEIDSigner/Properties/AssemblyInfo.cs
@@ -33,6 +33,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
-[assembly: NeutralResourcesLanguageAttribute("et")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
