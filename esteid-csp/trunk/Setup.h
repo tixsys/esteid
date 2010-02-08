17b267df014842843d9645db7f2b367bcaf5199d
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2010-02-08 14:10:11 +0000

    CSP: bumped version to 2.2.19

--- esteid-csp/trunk/Setup.h
+++ esteid-csp/trunk/Setup.h
@@ -17,10 +17,10 @@
 #define PACKAGE_NAME "EstEID CSP" PKG_BIT PKG_BUILD
 
 /* rc version */
-#define RC_VERSION 2,2,18
+#define RC_VERSION 2,2,19
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.2.18"
+#define PACKAGE_VERSION "2.2.19"
 
 /* Name of package */
 #define PACKAGE "esteidcsp.dll"
