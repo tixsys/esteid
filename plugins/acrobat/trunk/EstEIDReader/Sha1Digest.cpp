c9909ed6294e65d0c9a606a26a95b9308329ae98
anttix <anttix@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2010-03-25 13:36:56 +0000

    plugins/acrobat: updated copyright headers
    The code is copyright Estonian Informatics Centre. Original authors can
    be found in the AUTHORS file.

--- plugins/acrobat/trunk/EstEIDReader/Sha1Digest.cpp
+++ plugins/acrobat/trunk/EstEIDReader/Sha1Digest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009  Manuel Matonin <manuel@smartlink.ee>
+ * Copyright (C) 2009-2010  Estonian Informatics Centre
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,9 +13,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301  USA
- */ 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include "EstEIDIncludes.h"
 #include "Sha1Digest.h"
--- plugins/acrobat/trunk/EstEIDSigner/ErrorContainer.cs
+++ plugins/acrobat/trunk/EstEIDSigner/ErrorContainer.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2009  Manuel Matonin <manuel@smartlink.ee>
+ * Copyright (C) 2009-2010  Estonian Informatics Centre
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,8 +13,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 using System;
