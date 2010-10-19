1b5754a3c7fb399f977195fcad4a4964cfa4971f
kalevlember <kalevlember@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2010-10-19 18:44:50 +0000

    packages/macosx: remove more old files on install

--- macosx/trunk/Installer/uninstall.sh
+++ macosx/trunk/Installer/uninstall.sh
@@ -1,57 +1,73 @@
 #!/usr/bin/env bash
 
+if [ x"$(id -u)" != x"0" ] ; then
+	echo "You do not have enough privileges to run this script" 2>&1
+	echo "    run 'sudo $0'" 2>&1
+	exit 1
+fi
+
+# esteid-contextmenu
+echo 'Removing Finder plug-in'
+rm -rf /Library/Contextual\ Menu\ Items/EstEIDCM.plugin
+
+# esteid-digidoc
+echo 'Removing Digidoc libraries'
+rm -f /usr/local/etc/digidoc.conf
+rm -rf /usr/local/etc/digidocpp
+rm -rf /usr/local/share/libdigidoc
+rm -f /usr/local/lib/libdigidoc.dylib
+rm -f /usr/local/lib/libdigidoc.2.dylib
+rm -f /usr/local/lib/libdigidocpp.dylib
+rm -f /usr/local/lib/libdigidocpp.0.dylib
+rm -f /usr/local/lib/libp11.dylib
+rm -f /usr/local/lib/libp11.1.dylib
+
+# esteid-mozilla
 echo 'Removing Mozilla extension'
-rm -fR /Library/Application\ Support/Mozilla/Extensions/{3550f703-e582-4d05-9a08-453d09bdfdc6}/{aa84ce40-4253-11da-8cd6-0800200c9a66}
-rm -fR /Library/Application\ Support/Mozilla/Extensions/{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}/{aa84ce40-4253-11da-8cd6-0800200c9a66}
-rm -fR /Library/Application\ Support/Mozilla/Extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/{aa84ce40-4253-11da-8cd6-0800200c9a66}
+rm -rf /Library/Application\ Support/Mozilla/Extensions/*/\{aa84ce40-4253-11da-8cd6-0800200c9a66\}
+
+# esteid-opensc
+echo 'Removing OpenSC'
+rm -rf /Library/OpenSC
+rm -f /usr/lib/opensc-pkcs11.so
+
+# esteid-preferences
+echo 'Removing Preferences'
+rm -rf /Library/PreferencePanes/EstEIDPP.prefPane
+
+# esteid-qdigidocclient
+echo 'Removing Digidoc Client'
+rm -rf /Applications/qdigidocclient.app
+
+# esteid-qdigidoccrypto
+echo 'Removing Digidoc Crypto'
+rm -rf /Applications/Utilities/qdigidoccrypto.app
+
+# esteid-qesteidutil
+echo 'Removing ID-card Utility'
+rm -rf /Applications/qesteidutil.app
+
+# esteid-qt
+echo 'Removing Qt'
+rm -rf /Library/Frameworks/QtCore.framework
+rm -rf /Library/Frameworks/QtDBus.framework
+rm -rf /Library/Frameworks/QtGui.framework
+rm -rf /Library/Frameworks/QtNetwork.framework
+rm -rf /Library/Frameworks/QtWebKit.framework
+rm -rf /Library/Frameworks/QtXml.framework
+rm -rf /Library/Frameworks/QtXmlPatterns.framework 
+rm -rf /Library/Frameworks/phonon.framework
 
 echo 'Removing Updater'
-rm -fR /Library/Receipts/org.esteid.*
 rm -f /Library/LaunchAgents/org.esteid.updater.plist
-mv -f /Applications/Utilities/EstEIDSU.app ~/.Trash/ID-card\ Software
+rm -rf /Applications/Utilities/EstEIDSU.app
 
-echo 'Removing Preferences'
-mv -f /Library/PreferencePanes/EstEIDPP.prefPane ~/.Trash/ID-card\ Software
+echo 'Removing Web Browser plug-ins'
+rm -rf /Library/Internet\ Plug-Ins/esteid.plugin
+rm -rf /Library/Internet\ Plug-Ins/EstEIDWP.plugin
 
-echo 'Removing Safari plug-in'
-mv -f /Library/Internet\ Plug-Ins/esteid.plugin ~/.Trash/ID-card\ Software
-mv -f /Library/Internet\ Plug-Ins/EstEIDWP.plugin ~/.Trash/ID-card\ Software
-
-echo 'Removing Finder plug-in'
-mv -f /Library/Contextual\ Menu\ Items/EstEIDCM.plugin ~/.Trash/ID-card\ Software
-
-echo 'Removing OpenSC'
-mv -f /Library/OpenSC ~/.Trash/ID-card\ Software
-mv -f /usr/lib/opensc-pkcs11.so ~/.Trash/ID-card\ Software
-
-echo 'Removing ID-card Utility'
-mv -f /Applications/qesteidutil.app ~/.Trash/ID-card\ Software
-
-echo 'Removing Digidoc Client'
-mv -f /Applications/qdigidocclient.app ~/.Trash/ID-card\ Software
-
-echo 'Removing Digidoc Crypto'
-mv -f /Applications/Utilities/qdigidoccrypto.app ~/.Trash/ID-card\ Software
-
-echo 'Removing Digidoc libraries'
-mv -f /usr/local/etc/digidoc.conf ~/.Trash/ID-card\ Software
-mv -f /usr/local/etc/digidocpp ~/.Trash/ID-card\ Software
-mv -f /usr/local/share/libdigidoc ~/.Trash/ID-card\ Software
-mv -f /usr/local/lib/libdigidoc.dylib ~/.Trash/ID-card\ Software
-mv -f /usr/local/lib/libdigidoc.2.dylib ~/.Trash/ID-card\ Software
-mv -f /usr/local/lib/libdigidocpp.dylib ~/.Trash/ID-card\ Software
-mv -f /usr/local/lib/libdigidocpp.0.dylib ~/.Trash/ID-card\ Software
-mv -f /usr/local/lib/libp11.dylib ~/.Trash/ID-card\ Software
-mv -f /usr/local/lib/libp11.1.dylib ~/.Trash/ID-card\ Software
-
-
-echo 'Removing .plist files'
-mv -f ~/Library/Preferences/com.estonian-id-card.plist ~/.Trash/ID-card\ Software
-mv -f ~/Library/Preferences/org.esteid.* ~/.Trash/ID-card\ Software
-
-echo 'Emptying trash...'
-
-sudo rm -rf ~/.Trash/ID-card\ Software
+echo 'Removing receipts'
+rm -rf /Library/Receipts/org.esteid.*
 
 echo "Files removed."
 
