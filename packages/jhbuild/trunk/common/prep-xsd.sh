039922a2a856a1770c6b8a710481974fea79d1e1
anttix <anttix@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2010-11-17 16:53:51 +0000

    packages/jhbuild: Install XSD headers along with the binary

--- packages/jhbuild/trunk/common/prep-xsd.sh
+++ packages/jhbuild/trunk/common/prep-xsd.sh
@@ -3,9 +3,12 @@
 # Generate dummy Makefile to install xsd binary
 
 cat > $1/Makefile <<E_O_F
+INSTALL ?= install -p
+
 all:
 	echo "DO NOTHING"
 install:
 	\$(INSTALL) -d $2/bin
-	\$(INSTALL) -m 775 bin/xsd $2/bin/xsd
+	\$(INSTALL) -m 775 bin/xsd $2/bin/xsdcxx
+	install-headers "*.*" libxsd/xsd $2/include/xsd
 E_O_F
